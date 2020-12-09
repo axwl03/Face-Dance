06a1bf0dcc018bf0f64acef08bfc20991cf10471
axwl03 <arcticwarfare2711@gmail.com>
2020-12-09 13:37:41 +0800

    Add listener to netModule

--- netModule.py
+++ netModule.py
@@ -3,12 +3,11 @@ import fcntl
 import struct
 import netifaces as ni
 import threading
+import time
 
 class NetModule:
     def __init__(self, ui):
         self.ui = ui
-        self.__serverSocket = None
-        self.__clientSocket = None
         self.__isServer = False
 
     def isServer(self):
@@ -20,22 +19,30 @@ class NetModule:
     
     def connect(self, ip, port):
         self.__isServer = False
-        self.__clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__clientSocket.connect((ip, port))
+        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__socket.connect((ip, port))
         print('Connected')
-        self.__clientSocket.sendall(b'Hello World')
-        self.__clientSocket.close()
+
+#       self.__socket.sendall(b'Hello World')
+#       self.__socket.close()
 
     def listen(self, port):
         self.__isServer = True
         self.__serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__serverSocket.bind(('', port))
         self.__serverSocket.listen(1)
-        conn, addr = self.__serverSocket.accept()
-        print(conn.recv(1024).decode('utf-8'))
+        self.__socket, addr = self.__serverSocket.accept()
+        self.__listener = threading.Thread(target=self.run)
+        self.__listener.start()
         self.__serverSocket.close()
-        conn.close()
 
+#        print(self.__socket.recv(1024).decode('utf-8'))
+#        self.__socket.close()
+
+    def run(self):
+        for i in range(5):
+            print('listening')
+            time.sleep(1)
         
 mode = input() 
 if mode == 's':
