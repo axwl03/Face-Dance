5b00c3d27573c4a247724ca0574d6e207e87a804
axwl03 <arcticwarfare2711@gmail.com>
2020-12-23 15:55:36 +0800

    Add randomGenEmoji; Working on action in each turn

--- game.py
+++ game.py
@@ -3,8 +3,19 @@ from gamePage import Ui3_Form
 from netModule import NetModule
 import threading
 from emoji import Emoji
+import random
+import time
 
 class ThirdPage(QWidget):
+class ThirdPage():
+    maxEmojiNum = 8
+    maxEmojiGen = 1
+    maxX = 450
+    maxY = 500
+    myOffsetX = 500
+    myOffsetY = 200
+    duration = 30
+
     def __init__(self, isServer, ip):
         super(ThirdPage, self).__init__()
         self.ui = Ui3_Form()
@@ -20,6 +31,7 @@ class ThirdPage(QWidget):
         self.__port = 8080
 
         self.__net = NetModule(self)
+        self.__mutex = threading.Lock()
         self.__runner = threading.Thread(target=self.startGame)
         self.__runner.start()
 
@@ -37,6 +49,29 @@ class ThirdPage(QWidget):
                 self.__net.close()
                 break
         
+        startTime = time.time()
+        self.__action()
+
+    def __action(self):
+        if self.isServer == True:
+            # remove emoji if it exceeds boundary + 20
+            i = 0
+            while i < len(self.__myEmojiList):
+                if self.__myEmojiList[i].getY() > self.maxY + self.myOffsetY + 20:
+                    del self.__myEmojiList[i]
+                    continue
+                i = i + 1
+
+            # randomly generate emoji and append to myEmojiList
+            self.__randomGenEmoji()
+            self.__mutex.acquire()
+            for emoji in self.__emojiList:
+                newEmoji = Emoji(emoji.getX() + self.myOffsetX, emoji.getY() + self.myOffsetY, emoji.getType())
+                self.__myEmojiList.append(newEmoji)
+            self.__mutex.release()
+
+            # send exist emojiList and our score
+
     # set result that predicted by image module
     def setResult(self, emojiType):
         self.__matchedEmoji = emojiType
@@ -44,3 +79,19 @@ class ThirdPage(QWidget):
     # set face image which is obtained from webcam
     def setFaceImage(self, faceImage):
         self.__faceImage = faceImage
+
+    def __randomGenEmoji(self):
+        if len(self.__emojiList) >= self.maxEmojiNum:
+            return
+        else:
+            tempList = []
+            for i in range(self.maxEmojiGen):
+                tempList.append(Emoji(random.randint(0, self.maxX), 0, random.randint(1, Emoji.SURPRISE)))
+            self.__mutex.acquire()
+            self.__emojiList = tempList
+            self.__mutex.release()
+
+    def printEmojiList(self):
+        print('len: ', len(self.__myEmojiList))
+        for emoji in self.__myEmojiList:
+            print('{},{} {}'.format(emoji.getX(), emoji.getY(), emoji.getType()))
